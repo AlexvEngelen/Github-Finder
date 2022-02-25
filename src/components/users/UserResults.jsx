031f2d707d26cb62c56a4940e8518b8269dd6c2a
Alex van Engelen <alex.van.engelen@x-guard.nl>
2022-02-25 20:57:05 +0100

    Implemented github context.

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,29 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
+import GithubContext from '../../context/github/GithubContext';
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  // Retrieve data from context
+  const { users, fetchUsers, isLoading } = useContext(GithubContext);
 
   useEffect(() => {
+    // Get users from API on render
     fetchUsers();
   }, []);
 
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-
-    const data = await response.json();
-
-    setUsers(data);
-    setIsLoading(false);
-  };
-
   if (!isLoading) {
+    // Show users when data received from github api
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
         {users.map((user) => (
@@ -32,6 +22,7 @@ function UserResults() {
       </div>
     );
   } else {
+    // Show spinner when loading
     return <Spinner />;
   }
 }
